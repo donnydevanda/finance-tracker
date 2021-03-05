2900b5d7f790d77ff7534bc6c2eab904ec9aabd5
donnydevanda <devandadonny@gmail.com>
2021-03-05 16:11:29 +0700

    stock add delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def my_portfolio
-
+    @tracked_stock = current_user.stocks
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: '1@md.com', password: '123123')
+User.create(email: '2@md.com', password: '123123')
+User.create(email: '3@md.com', password: '123123')
+
+Stock.create(name: 'Apple.Inc', ticker: 'AAPL')
+Stock.create(name: 'Microsoft', ticker: 'MSFT')
+Stock.create(name: 'Tesla', ticker: 'TSLA')
+
+UserStock.create(user_id: 1, stock_id: 1)
+UserStock.create(user_id: 1, stock_id: 2)
+UserStock.create(user_id: 1, stock_id: 3)
+UserStock.create(user_id: 2, stock_id: 1)
+UserStock.create(user_id: 2, stock_id: 2)
+UserStock.create(user_id: 2, stock_id: 3)
+UserStock.create(user_id: 3, stock_id: 1)
+UserStock.create(user_id: 3, stock_id: 2)
+UserStock.create(user_id: 3, stock_id: 3)
