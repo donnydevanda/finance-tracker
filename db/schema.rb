e0e852bdc2a59b491cc9e131411c51cea377e55e
Muhammad Donny Devanda <devandadonny@gmail.com>
2021-03-09 09:06:40 +0700

    final

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_092456) do
+ActiveRecord::Schema.define(version: 2021_03_08_020952) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "friend_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
 
   create_table "stocks", force: :cascade do |t|
     t.string "ticker"
@@ -32,6 +41,8 @@ ActiveRecord::Schema.define(version: 2021_03_05_092456) do
 # Could not dump table "users" because of following StandardError
 #   Unknown type 'sting' for column 'last_name'
 
+  add_foreign_key "friendships", "users"
+  add_foreign_key "friendships", "users", column: "friend_id"
   add_foreign_key "user_stocks", "stocks"
   add_foreign_key "user_stocks", "users"
 end
